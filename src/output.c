c87c0623db9b3e7ffb0d155c0561bd9c2f5e882e
Joshua Pepple <pepplejoshuatams@gmail.com>
2023-12-24 00:34:07 -0700

    Add macros, types, and prototypes to output.c

--- src/output.c
+++ src/output.c
@@ -1,4 +1,26 @@
 #include "c.h"
 
-// exported functions
-extern void outs ARGS_();
+// macros
+
+// types
+
+// prototypes
+extern void outs ARGS((char *));
+extern void print ARGS((char *, ...));
+
+// data
+// functions
+// void outs(char *s) {
+//   char *p;
+//   for (p = bp; (*p = *s++) != 0; p++) {}
+//   bp = p;
+//   if (bp > io[fd]->limit) outflush();
+// }
+
+void print VARARGS((char *fmt, ...), (fmt, va_alist), va_dcl) {
+  va_list ap;
+
+  va_init(ap, fmt);
+  vprint(fmt, ap);
+  va_end(ap);
+}
