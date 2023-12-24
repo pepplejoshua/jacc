8305daa3fbc7f670d595fd975647cee88c8df16a
Joshua Pepple <pepplejoshuatams@gmail.com>
2023-12-23 23:11:55 -0700

    Add c.h header file

---
+++ include/c.h
@@ -0,0 +1,12 @@
+#include <stdarg.h>
+
+// exported macros
+#define ARGS_(list) list
+#define va_init(a,b) va_start(a,b)
+
+// typedefs
+// #include "config.h"
+// interfaces (page 78)
+// exported types
+// exported data
+// exported functions
